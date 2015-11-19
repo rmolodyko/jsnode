27566420e4c3bc98558cc7215c52c28c390a7672
rmolodyko <molodyko13@gmail.com>
2015-11-19 02:19:40 +0200

    Trying jade

--- server.js
+++ server.js
@@ -8,23 +8,39 @@ var app = express();
 var route = express.Router();
 var route1 = express.Router();
 
-route1.get('/u', function(req, res){
-    res.send('awesome');
+app.set('views', './server/jade');
+app.set('view engine', 'jade');
+
+var data = [
+    {name: 'Home', url: '/', body: 'Some body', title: 'Home'},
+    {name: 'About', url: '/about', body: 'About us', title: 'About'}
+];
+
+app.get('/', function(req, res) {
+    res.render('index', {'data': data, 'current': data[0]});
 });
 
-route.use(function(req, res, next) {
-    next();
+app.get('/about', function(req, res) {
+    res.render('index', {'data': data, 'current': data[1]});
 });
 
-route.use('/g', route1);
-
-route.get(/^\/h.*1$/, function(req, res) {
-    console.log('do some action');
-    res.send('is the end');
-});
-
-// Assign custom router for work over the api
-app.use('/api', route);
+//route1.get('/u', function(req, res){
+//    res.send('awesome');
+//});
+//
+//route.use(function(req, res, next) {
+//    next();
+//});
+//
+//route.use('/g', route1);
+//
+//route.get(/^\/h.*1$/, function(req, res) {
+//    console.log('do some action');
+//    res.send('is the end');
+//});
+//
+//// Assign custom router for work over the api
+//app.use('/api', route);
 
 // Start server
 app.listen(3000);
